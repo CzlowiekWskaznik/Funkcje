dfe0fb5752d1ede526e6994ba9ae7af0e674875d
elpenny <mikipage@gmail.com>
2016-01-31 13:59:50 +0000

    dodane dzielenie do glownej petli

--- funkcje.c
+++ funkcje.c
@@ -2,6 +2,7 @@
 #include "dodawanie.h"
 #include "odejmowanie.h"
 #include "mnozenie.h"
+#include "Dzielenie.h"
 
 int main()
 {
@@ -12,6 +13,7 @@ float b = 6.23;
 printf("Dodawanie to: %f\n",dodawanie(a,b));
 printf("Odejmowanie to: %f\n",odejmowanie(a,b));
 printf("Mnozenie to: %f\n",mnozenie(a,b));
+printf("Dzielenie to: %f\n",dzielenie(a,b));
 
 return 0;
 }
